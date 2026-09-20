027041f72c3a4655644e77e380efb0bc8a663d68
Anmol1578 <anmol10562@gmail.com>
2026-09-20 20:32:39 +0530

    add ActivityIndicator component for loading indicators

---
+++ Components/09_ActivityIndicator.tsx
@@ -0,0 +1,18 @@
+import { View, ActivityIndicator } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        padding: 60,
+      }}
+    >
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="large" color="midnightblue" />
+      <ActivityIndicator size="large" color="midnightblue" animating={false} />
+    </View>
+  );
+}
---
+++ Components/10_Alert.tsx
@@ -0,0 +1,36 @@
+import { View, Alert, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        padding: 60,
+      }}
+    >
+      <Button title="Alert" onPress={() => Alert.alert("Invalid Data!")} />
+      <Button
+        title="Alert 2"
+        onPress={() => Alert.alert("Invalid Data!", "DOB Incorrect")}
+      />
+
+      <Button
+        title="Alert 3"
+        onPress={() =>
+          Alert.alert("Invalid Data!", "DOB Incorrect", [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+            },
+
+            {
+              text: "OK",
+              onPress: () => console.log("OK Pressed"),
+            },
+          ])
+        }
+      />
+    </View>
+  );
+}
