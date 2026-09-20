5adcc5fcbce0320568d8d6aacdf38a33b9807168
Anmol1578 <anmol10562@gmail.com>
2026-09-20 21:00:42 +0530

    add multiple components with styled boxes for enhanced UI display

---
+++ Components/11_StyleSheet_API.tsx
@@ -0,0 +1,23 @@
+import { View, Text, Button, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>StyleSheet API</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 60,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
